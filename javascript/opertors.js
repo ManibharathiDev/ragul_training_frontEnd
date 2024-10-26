09450021a6d3d58472e21df40ede9f85f07f2152
Manibharathi <manibharath159@gmail.com>
2024-10-26 19:05:23 +0530

    Relational

---
+++ javascript/opertors.js
@@ -0,0 +1,38 @@
+//Relational Operators
+//a = b+c // + Operator , a,b,c Operand
+// >, <, ==,!=,>=,<=
+// > Greater Than
+// < Less than
+// >= Greater or Equal
+// <= Less or Equal
+// == Equal to
+// != Not Equal
+//========================================
+console.log("Greater Than");
+console.log(10 > 6);
+console.log(5 > 10);
+console.log(15>10); //true
+console.log(15>90); // false
+console.log(90>78); // true
+console.log("Less Than");
+console.log(8<12);
+console.log(13<5);
+
+console.log("Greater or Equal");
+
+console.log(12>=12);
+console.log(15>=12);
+console.log(15>=90);
+
+console.log("Less or Equal");
+console.log(15<=90);
+console.log(90<=89);
+
+console.log("Equal to");
+console.log(10==10);
+console.log(10==90);
+
+console.log("Not Equal");
+console.log(10!=90);
+console.log(90!=90);
+
