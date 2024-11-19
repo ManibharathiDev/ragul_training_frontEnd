f4a6d7987b82fce0b6a0b35b80a24537f216f7cd
Manibharathi <manibharath159@gmail.com>
2024-11-19 19:03:54 +0530

    If,else,Conditional

---
+++ javascript/if.js
@@ -0,0 +1,16 @@
+let a = 10;
+console.log("welcome");
+//Check if the given number is greater than 5
+if(a>5)
+{
+    console.log("A is greater than 5");
+}
+
+console.log("Hi");
+
+// Check if the given number is less than 50;
+let b = 45;
+if(b<50)
+{
+    console.log("B is less than 50");
+}
---
+++ javascript/ifelse.js
@@ -0,0 +1,11 @@
+let a = 15;
+if(a>5)
+{
+    console.log("A is greater than 5");
+}
+else
+{
+    console.log("A is not a greater than 5");
+}
+
+//check if the give number is less thatn 50
