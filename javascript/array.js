1f79072553043239cf2491e8a5c50a2cce34c05a
Manibharathi <manibharath159@gmail.com>
2024-11-26 19:07:03 +0530

    Array

---
+++ javascript/array.js
@@ -0,0 +1,38 @@
+let age = [];
+age[0] = 23;
+age[1] = 45;1
+age[2] = 20;
+age[3] = 12;
+
+console.log(age[3]);
+console.log(age[2]);
+
+let names = ["Ragul","Jack","Pavi","Daniel"];
+//console.log(names[2])
+//Array Access
+// Direct Index
+// Looping
+for(let i = 0;i<=3;i++)
+{
+    console.log(names[i]);
+}
+
+// Sum of the array
+let cost = [10,20,45,90,100];
+let sum = 0;
+for(let i = 0;i<=4;i++)
+{
+    sum = sum+cost[i];
+}
+console.log(sum);
+
+//mult of the array;
+let b = [45,68,13,46,89];
+let mult = 0;
+for(let i = 0; i<=4;i++)
+{
+    mult = mult*b[i];
+}
+console.log(mult);
+
+// Odd Number,Even Number
